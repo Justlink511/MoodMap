ff695667fdd3cc21c41dfa141669d673fa1e798a
Thibault Vivier <thibault.vivier@petitpierre.ch>
2025-06-06 15:08:50 +0200

    deploy: export statique Next.js

---
+++ _next/static/_ZweUE5HRsn2J-cOzYUgG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/265-8405cfecee3652bc.js","static/chunks/pages/index-1a335055703ca2f6.js"],"/_error":["static/chunks/pages/_error-9dac069270de162a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
