2e241eb3a812e5d0827e027ed7ba956805bd4582
Thibault Vivier <thibault.vivier@petitpierre.ch>
2025-06-06 10:54:13 +0200

    fix: configuration des ressources statiques

--- next.config.js
+++ next.config.js
@@ -15,7 +15,23 @@ const nextConfig = {
   // Forcer le mode statique
   experimental: {
     appDir: false,
-  }
+  },
+  // Configuration pour les ressources statiques
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          publicPath: '/MoodMap/_next/static/media/',
+          outputPath: 'static/media/',
+          name: '[name].[hash].[ext]',
+        },
+      },
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
