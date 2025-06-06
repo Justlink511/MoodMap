31423cf74bc3331510ac47647d16a44a6e0570e1
Thibault Vivier <thibault.vivier@petitpierre.ch>
2025-06-06 07:53:30 +0200

    Mise à jour de la configuration pour le déploiement

--- next.config.js
+++ next.config.js
@@ -4,8 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/mood-map' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/mood-map/' : '',
+  basePath: '/mood-map',
+  assetPrefix: '/mood-map/',
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
