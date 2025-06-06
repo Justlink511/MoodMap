0cbeeea522f995ce2e94d360b5afba2f9bba5a83
Thibault Vivier <thibault.vivier@petitpierre.ch>
2025-06-06 08:06:13 +0200

    Simplification de la configuration pour le déploiement

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/mood-map',
-  assetPrefix: '/mood-map/',
+  basePath: '',
+  assetPrefix: '',
   images: {
     unoptimized: true,
   },
