a2e82b44cef5785e996997bf02d216dc43028887
Thibault Vivier <thibault.vivier@petitpierre.ch>
2025-07-21 15:35:51 +0200

    Updates

---
+++ _next/static/yAYbD_K0mieiwGccowzqy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/265-5c413032ffdd2373.js","static/chunks/pages/index-fdbb4e6ebffa7b0f.js"],"/_error":["static/chunks/pages/_error-9dac069270de162a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
